ca6a5af7d23795321b34d8e3c18c71538c45ff12
Rom Vic <technoyguy@gmail.com>
2018-08-03 13:36:02 -0700

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Where.Cash</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
