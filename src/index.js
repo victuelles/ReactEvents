e81b4ff96979f994d3b23ccbb19879d92fb964c7
Rom Vic <technoyguy@gmail.com>
2018-08-04 14:00:14 -0700

    Redux added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import App from './app/layout/App.jsx';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
 
-const rootEl= document.getElementById('root')
+const store = configureStore();
 
-let render=()=>{
-    ReactDOM.render(<App />, rootEl);
-}
+const rootEl = document.getElementById('root');
 
-if(module.hot){
-    module.hot.accept('./app/layout/App',()=>{
-        setTimeout(render)
-    })
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
 render();
